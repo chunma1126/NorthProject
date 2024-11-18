c7779e007be7a7f8a932e4f0a2877538a7318ddf
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2024-11-18 12:18:22 +0900

    Add Scene

--- 2024_winapigamep_framework_22/BossScene.cpp
+++ 2024_winapigamep_framework_22/BossScene.cpp
@@ -1,22 +1,20 @@
 #include "pch.h"
 #include "BossScene.h"
 #include "ResourceManager.h"
-#include "Player.h"
+#include "Projectile.h"
 
 void BossScene::Init()
 {
-	Object* pObj = new Player;
+	Object* pObj = new Projectile;
 	pObj->SetPos({ SCREEN_WIDTH / 2.f,150.f });
 	pObj->SetSize({ 100.f,100.f });
-	pObj->SetName(L"Player");
-	AddObject(pObj, LAYER::PLAYER);
+	pObj->SetName(L"Projectile");
+	AddObject(pObj, LAYER::PROJECTILE);
 
 
 
 
-}
-
-void BossScene::Update()
-{
 
 }
+
+
