1331dfab854154441082f775fad65859a068d5f1
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2024-12-01 16:00:04 +0900

    Add Background , Fix Background Texture

---
+++ 2024_winapigamep_framework_22/Background.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "Background.h"
+#include "ResourceManager.h"
+#include "Texture.h"
+#include "TimeManager.h"
+Background::Background()
+{
+	m_pTexture = GET_SINGLE(ResourceManager)->TextureLoad(L"Background", L"Texture\\Background.bmp");
+}
+
+Background::~Background()
+{
+}
+
+void Background::Update()
+{
+    Vec2 curPos = GetPos();
+
+    SetPos({ curPos.x , curPos.y + m_scrollingSpeed * fDT});
+
+    if (curPos.y >= 1080)
+        SetPos({0,0});
+
+}
+
+void Background::Render(HDC _hdc)
+{
+    Vec2 pos = GetPos();
+
+
+    int width = SCREEN_WIDTH;  
+    int height = SCREEN_HEIGHT;
+
+    ::BitBlt(_hdc
+        , pos.x 
+        , pos.y - SCREEN_HEIGHT
+        , width, height,
+        m_pTexture->GetTexDC()
+        , 0, 0, SRCCOPY);
+
+    ::BitBlt(_hdc
+        , pos.x
+        , pos.y
+        , width, height,              
+        m_pTexture->GetTexDC()
+        , 0, 0,SRCCOPY);
+}
+
--- 2024_winapigamep_framework_22/UIManager.cpp
+++ 2024_winapigamep_framework_22/UIManager.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "UIManager.h"
 #include "UI.h"
+#include "SceneManager.h"
+
 
 void UIManager::Init()
 {
@@ -9,11 +11,12 @@ void UIManager::Init()
 
 void UIManager::Update()
 {
+	
 }
 
 void UIManager::Render(HDC _hdc)
 {
-	
+	RECT_RENDER(_hdc,SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 , 10 , 10);
 }
 
 void UIManager::AddChild(wstring _key,UI* _newUI)
