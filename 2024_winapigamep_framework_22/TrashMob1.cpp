fd72d97d407824a279f9778a8522a7c8aca215be
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2024-12-02 12:47:43 +0900

    Fix TrasjMob_1

--- 2024_winapigamep_framework_22/TrashMob1.cpp
+++ 2024_winapigamep_framework_22/TrashMob1.cpp
@@ -3,58 +3,34 @@
 #include "Collider.h"
 #include "EventManager.h"
 #include "TimeManager.h"
+#include "Animator.h"
+#include "Texture.h"
+#include "ResourceManager.h"
 
-TrashMob1::TrashMob1()
-	: m_hp(5)
+
+TrashMob1::TrashMob1(const wstring& _key, const wstring& _path)
 {
-	this->AddComponent<Collider>();
+	m_texture = GET_SINGLE(ResourceManager)->TextureLoad(L"Enemy_1" , L"Texture\\Enemy_1");
+
+
+	AddComponent<Animator>();
+	GetComponent<Animator>()->CreateAnimation(L"Enemy_1", m_texture, { 0,0 }, { 15,16 }, { 1,0 }, 5, 1);
+	GetComponent<Animator>()->PlayAnimation(L"Enemy_1", true);
+
+	AddComponent<Collider>();
 }
 
 TrashMob1::~TrashMob1()
 {
+
 }
 
 void TrashMob1::Update()
 {	 
-	//Vec2 dir = ifPlayerPos - GetPos();
 	Vec2 down = { 0.f,40*fDT };
 	Vec2 curPos = GetPos();
 	SetPos(curPos + down);
-
 }	 
 	 
-void TrashMob1::Render(HDC _hdc)
-{
-	//HBRUSH brush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
-	//HBRUSH oldbrush = (HBRUSH)SelectObject(_hdc, brush);
-	Vec2 vPos = GetPos();
-	Vec2 vSize = GetSize();
-	RECT_RENDER(_hdc, vPos.x, vPos.y
-		, vSize.x, vSize.y);
-	ComponentRender(_hdc);
-	//SelectObject(_hdc, oldbrush); 
-	//DeleteObject(brush);
-}
 
-void TrashMob1::EnterCollision(Collider* _other)
-{
-	std::cout << "Enter" << std::endl;
-	Object* pOtherObj = _other->GetOwner();
-	wstring str = pOtherObj->GetName();
-	if (pOtherObj->GetName() == L"PlayerBullet")
-	{
-		m_hp -= 1;
-		if (m_hp <= 0)
-			GET_SINGLE(EventManager)->DeleteObject(this);
-	}
-}
 
-void TrashMob1::StayCollision(Collider* _other)
-{
-	//std::cout << "Stay" << std::endl;
-}
-
-void TrashMob1::ExitCollision(Collider* _other)
-{
-	std::cout << "Exit" << std::endl;
-}
--- 2024_winapigamep_framework_22/TrashMob1.h
+++ 2024_winapigamep_framework_22/TrashMob1.h
@@ -1,18 +1,15 @@
 #pragma once
 #include "Enemy.h"
+class Texture;
 class TrashMob1 : public Enemy
 {
 public:
-	TrashMob1();
+	TrashMob1(const wstring& _key, const wstring& _path);
 	~TrashMob1();
 public:
 	void Update() override;
-	void Render(HDC _hdc) override;
-public:
-	virtual void EnterCollision(Collider* _other);
-	virtual void StayCollision(Collider* _other);
-	virtual void ExitCollision(Collider* _other);
+
 private:
-	int m_hp;
+
 };
 
