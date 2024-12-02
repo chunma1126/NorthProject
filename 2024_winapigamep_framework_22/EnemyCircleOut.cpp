bb73dcf27e57511af07d231ba29dbe1d65bae454
zhdltl601 <zhdltl601@gmail.com>
2024-12-02 10:51:43 +0900

    Added EnemyCircleOut

---
+++ 2024_winapigamep_framework_22/EnemyCircleOut.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "EnemyCircleOut.h"
+
+EnemyCircleOut::EnemyCircleOut(const wstring& _key, const wstring& _path)
+	:Enemy(_key, _path)
+{
+
+}
+EnemyCircleOut::~EnemyCircleOut()
+{
+}
+
