345b56d1ba0ab4114f3c9363c7088a9c54488b00
zhdltl601 <zhdltl601@gmail.com>
2024-12-02 10:09:35 +0900

    my scene update

--- 2024_winapigamep_framework_22/OJYScene.cpp
+++ 2024_winapigamep_framework_22/OJYScene.cpp
@@ -5,20 +5,30 @@
 #include "CollisionManager.h"
 #include "ResourceManager.h"
 #include "Projectile.h"
+#include "AllEnemies.h"
+#include "DelayedCall.h"
 void OJYScene::Init()
 {
+	//DelayedCall* d = new DelayedCall(2.f, { 1 });
+	
 	Object* pObj = new Enemy;
 	pObj->SetPos({ SCREEN_WIDTH / 2.f, 150.f });
 	pObj->SetSize({ 100.f,100.f });
-	pObj->SetName(L"Enemy");
 	AddObject(pObj, LAYER::ENEMY);
 
-	Projectile* pEnemyProj = new Projectile(L"EnemyBullet", L"Texture\\Heart.bmp");
-	pEnemyProj->SetPos({ SCREEN_WIDTH / 2.f, 200.f });
-	pEnemyProj->SetSize({ 30.f,30.f });
-	pEnemyProj->SetSpeed(30);
-	pEnemyProj->SetTag(TagEnum::EnemyProjectile);
-	AddObject(pEnemyProj, LAYER::PROJECTILE);
+	//Projectile* pEnemyProj = new Projectile(L"EnemyBullet", L"Texture\\Heart.bmp");
+	//pEnemyProj->SetPos({ SCREEN_WIDTH / 2.f, 200.f });
+	//pEnemyProj->SetSize({ 30.f,30.f });
+	//pEnemyProj->SetSpeed(30);
+	//pEnemyProj->SetTag(TagEnum::EnemyProjectile);
+	//AddObject(pEnemyProj, LAYER::PROJECTILE);
+
+	Projectile* pEnemyProj2 = new Projectile(L"EnemyBullet", L"Texture\\Heart.bmp");
+	pEnemyProj2->SetPos({ SCREEN_WIDTH / 2.f, 250.f });
+	pEnemyProj2->SetSize({ 30.f,30.f });
+	pEnemyProj2->SetSpeed(0);
+	pEnemyProj2->SetTag(TagEnum::EnemyProjectile);
+	AddObject(pEnemyProj2, LAYER::PROJECTILE);
 
 	Object* pPlayer = new Player;
 	pPlayer->SetPos({ SCREEN_WIDTH / 2.f, SCREEN_HEIGHT * 2.f / 3.f });
