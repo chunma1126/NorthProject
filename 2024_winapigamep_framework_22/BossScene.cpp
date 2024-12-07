6cfa2432f140d7f80f7786f9d01bf77b5ba6f504
zhdltl601 <zhdltl601@gmail.com>
2024-12-07 18:07:18 +0900

    bosss scene

--- 2024_winapigamep_framework_22/BossScene.cpp
+++ 2024_winapigamep_framework_22/BossScene.cpp
@@ -28,6 +28,8 @@ void BossScene::Init()
 	//DelayedCall* delayedCall0 = new DelayedCall(2.f, { {SCREEN_WIDTH * 0.2f , 0.f }, EnemyType::TrashMob1, L"EnemySheetBlue", L"Texture\\EnemySheet_Blue.bmp", 10 });
 	//DelayedCall* delayedCall1 = new DelayedCall(2.f, { {SCREEN_WIDTH * 0.8f , 0.f }, EnemyType::TrashMob1, L"EnemySheetBlue", L"Texture\\EnemySheet_Blue.bmp", 10 });
 
+
+
 	DelayedCall* midBoss = new DelayedCall(0.f, { {SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f}, EnemyType::MidBoss, L"EnemySheetBlue", L"Texture\\EnemySheet_Blue.bmp", 10 });
 
 	//GET_SINGLE(BulletManager)->HeartDataInit(270.f);
