ab46c093aa7188431c6e06eb61f7064d68aff1aa
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2024-12-07 20:31:05 +0900

    Add Boss Texture and Add Boss Rendering

--- 2024_winapigamep_framework_22/MidBoss.cpp
+++ 2024_winapigamep_framework_22/MidBoss.cpp
@@ -7,14 +7,18 @@
 #include "ResourceManager.h"
 #include "StateMachine.h"
 #include "State.h"
+#include "Texture.h"
 MidBoss::MidBoss(const wstring& _key, const wstring& _path)
 	: Enemy(_key, _path)
 {
 	m_stateMachine = new StateMachine();
+
+	m_texture = GET_SINGLE(ResourceManager)->TextureLoad(L"Boss", L"Texture\\Boss.bmp");
 }
 
 MidBoss::~MidBoss()
 {
+
 }
 
 void MidBoss::Update()
@@ -28,6 +32,21 @@ void MidBoss::Update()
 	{
 		Shot();
 	}
+}
+void MidBoss::Render(HDC _hdc)
+{
+	Vec2 vPos = GetPos();
+	Vec2 vSize = GetSize();
+
+	int width = m_texture->GetWidth();
+	int height = m_texture->GetHeight();
+
+	::TransparentBlt(_hdc
+		, (int)(vPos.x - width / 2)
+		, (int)(vPos.y - height / 2)
+		, width + vSize.x / 2, height + vSize.y / 2,
+		m_texture->GetTexDC()
+		, 0, 0, width, height, RGB(255, 0, 255));
 }
 void MidBoss::Shot()
 {
