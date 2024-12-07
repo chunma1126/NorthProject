0d239bd4e605fb4be0945c3adefb60c3f5b20021
zhdltl601 <zhdltl601@gmail.com>
2024-12-07 18:06:58 +0900

    miniboss states

---
+++ 2024_winapigamep_framework_22/MBS_p1.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "MBS_p1.h"
+
+void MBS_p1::Enter()
+{
+	cout << "enter";
+}
+
+void MBS_p1::Update()
+{
+	cout << "Upd";
+}
+
+void MBS_p1::Exit()
+{
+	cout << "exot";
+}
---
+++ 2024_winapigamep_framework_22/MBS_p1.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "State.h"
+class MBS_p1 : public State
+{
+	void Enter() override;
+	void Update() override;
+	void Exit() override;
+};
+
