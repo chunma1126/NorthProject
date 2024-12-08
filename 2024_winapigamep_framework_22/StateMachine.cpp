d6a8452834d45ca0f5b95e7daa239ea6e6496672
zhdltl601 <zhdltl601@gmail.com>
2024-12-08 22:21:24 +0900

    State Machine

--- 2024_winapigamep_framework_22/StateMachine.cpp
+++ 2024_winapigamep_framework_22/StateMachine.cpp
@@ -6,6 +6,9 @@ StateMachine::StateMachine(MidBoss* midBoss)
 {
 	m_stateHT[MidBossState::p1] = new MBS_p1(midBoss);
 	m_stateHT[MidBossState::p2] = new MBS_p2(midBoss);
+	m_stateHT[MidBossState::p3] = new MBS_p3(midBoss);
+	m_stateHT[MidBossState::p4] = new MBS_p4(midBoss);
+	m_stateHT[MidBossState::p5] = new MBS_p5(midBoss);
 	m_currentState = m_stateHT[MidBossState::p1];
 	m_currentState->Enter();
 }
