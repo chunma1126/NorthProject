abec29a45d0b0068e0dd503edecafd868ee5d66d
zhdltl601 <zhdltl601@gmail.com>
2024-12-09 00:39:31 +0900

    Player God mode set to false

--- 2024_winapigamep_framework_22/Player.cpp
+++ 2024_winapigamep_framework_22/Player.cpp
@@ -21,13 +21,11 @@ Player::Player()
 	: m_pTex(nullptr)
 {
 	//sound
-	{
 		GET_SINGLE(ResourceManager)->LoadSound(L"PlayerShoot", L"Sound\\PlayerShot.wav", false);
 		GET_SINGLE(ResourceManager)->LoadSound(L"PlayerHit", L"Sound\\PlayerHit.wav", false);
 		GET_SINGLE(ResourceManager)->LoadSound(L"PlayerDead", L"Sound\\PlayerDead.wav", false);
 		GET_SINGLE(ResourceManager)->LoadSound(L"PlayerPowerUp", L"Sound\\PowerUp.wav", false);
 		GET_SINGLE(ResourceManager)->LoadSound(L"GameOver", L"Sound\\GameOver.wav", false);
-	}
 	
 	//texture
 	{
