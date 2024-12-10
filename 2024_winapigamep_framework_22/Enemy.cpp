90dbca94e3a6f571dde49cbb9925de50db31a556
zhdltl601 <zhdltl601@gmail.com>
2024-12-10 21:30:54 +0900

    Enemy Item Drop Value Decreased

--- 2024_winapigamep_framework_22/Enemy.cpp
+++ 2024_winapigamep_framework_22/Enemy.cpp
@@ -61,7 +61,7 @@ void Enemy::Update()
 		std::uniform_int_distribution<int> random(0, 100);
 		int rand = random(m_mt);
 
-		if (rand < 45)
+		if (rand < 35)
 		{
 			Item* item = new Item;
 			item->SetPos(GetPos());
