86b8a1d9047eef521a170fdd5ebda3c56f4efdab
zhdltl601 <zhdltl601@gmail.com>
2024-12-10 23:03:18 +0900

    Added easy scene, + filter

---
+++ 2024_winapigamep_framework_22/EasyScene.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Scene.h"
+class DelayedCall;
+class Player;
+class Background;
+class EasyScene : public Scene
+{
+public:
+	~EasyScene();
+	void Init() override;
+	void Update() override;
+public:
+private:
+	Player* m_player;
+	Background* m_backGround;
+};
+
